c72b7d6051bba024bf1cfbd7cdc1c0b35b967d57
mukhid10 <mukhid976@gmail.com>
2022-06-22 09:57:19 +0700

    init

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/pictures", express.static(path.join(__dirname, "pictures")));
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("hei welcome in APi amo");
+});
+
 //allRouter
 const allRouter = require("./routes/index");
 app.use(allRouter);
